3a4fd945fcacbe4274702236351d78cae7f31bda
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2024-09-07 02:20:06 +0600

    Video Updated

--- src/pages/Home/LandingBanner.jsx
+++ src/pages/Home/LandingBanner.jsx
@@ -6,7 +6,7 @@ export default function LandingBanner() {
     <section className='relative'>
         <video className='w-full h-full' src={videoBg} autoPlay loop muted />
         <div className='absolute top-0 left-0 bg-black/60 h-full w-full flex justify-center items-center'>
-            {/* <h1 className='text-white text-3xl md:text-6xl font-extrabold'>Discover Your Place To Live</h1> */}
+            {/* <h1 className='text-white text-3xl md:text-6xl font-extrabold'>Nazari Property Group</h1> */}
         </div>
     </section>
   )
